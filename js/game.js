7b1ac2cfc3f2de9f57df72cd8d7fdd4b48b49bc2
Preston Valles <pvalles2019@gmail.com>
2021-03-10 12:12:14 -0700

    Game layout added

--- js/game.js
+++ js/game.js
@@ -1,3 +1,14 @@
-function msg(){
- alert("Hello Javatpoint");
-}  
+window.onload = function() {
+
+  var i = 0;
+  var titleTextHolder = "Welcome to Javascript RPG"
+  var speed = 50;
+  function titleWriter() {
+    if (i < titleTextHolder.length) {
+      document.getElementById("titleText").innerHTML += titleTextHolder.charAt(i);
+      i++;
+      setTimeout(titleWriter, speed);
+    }
+  }
+  titleWriter();
+}
