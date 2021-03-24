e532f59c4e8a58c3efd8e59f775ac1ac8f40d537
MitchellT1025 <tylermitchell536@u.boisestate.edu>
2021-03-24 14:33:25 -0600

    Update random.js

--- js/random.js
+++ js/random.js
@@ -1,14 +1,20 @@
-
-
 window.onload = function(){
   var randomNumber = Math.floor(Math.random() * 100);
   document.getElementById("random_number").innerHTML = randomNumber;
 }
+
 const randomBtn = document.getElementById('randBtn');
 randomBtn.addEventListener('click', random_Number);
+
+
+function randon_number(){
+  var randomNumber = Math.floor(Math.random() * 100);
+  document.getElementById("randBtn").innerHTML = randomNumber;
+  randomBtn.removeEventListener('click' , random_number)
+}
 
 
 const htmlButton = document.getElementById('htmlBtn');
 htmlButton.addEventListener('click', htmlClick);
 const cssButton = document.getElementById('cssBtn');
-cssButton = document.getElementById('click', cssClick); 
+cssButton = document.getElementById('click', cssClick);
