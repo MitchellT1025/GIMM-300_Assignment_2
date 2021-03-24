e41247446d4f3e3a24ea4c9ae559c4952e00aabf
WitcherHawke <62607565+WitcherHawke@users.noreply.github.com>
2021-03-24 14:10:48 -0600

    stuff

--- js/random.js
+++ js/random.js
@@ -1,4 +1,18 @@
+const htmlButton = document.getElementById('htmlBtn');
+const cssButton = document.getElementById('cssBtn');
+htmlButton.addEventListener('click', htmlClick);
+cssButton.addEventListener('click', cssClick);
+
+
 window.onload = function() {
   var randomNumber = Math.floor(Math.random() * 100);
   document.getElementById("random_number").innerHTML = randomNumber;
+}
+
+function htmlClick(){
+
+}
+
+function cssClick(){
+
 }
