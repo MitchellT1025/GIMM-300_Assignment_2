caa1af41d86685a31e52575f9870bbb29a3fb7a5
WitcherHawke <62607565+WitcherHawke@users.noreply.github.com>
2021-03-24 16:34:32 -0600

    yes

--- js/random.js
+++ js/random.js
@@ -1,6 +1,10 @@
+const img = document.getElementById('Ricky');
+
 window.onload = function(){
   var randomNumber = Math.floor(Math.random() * 100);
   document.getElementById("random_number").innerHTML = randomNumber;
+
+  img.style.visibility = 'hidden';
 }
 
 const randomBtn = document.getElementById("randBtn");
@@ -20,8 +24,9 @@ const cssButton = document.getElementById('cssBtn');
 cssButton.addEventListener('click', cssClick);
 
 function htmlClick(){
-
+img.style.visibility = 'visible';
 }
+
 
 function cssClick(){
   cssButton.style.backgroundColor = 'red';
