9e7842234a13c8ce72043c593ea906844fa4000e
Preston Valles <pvalles2019@gmail.com>
2021-03-24 12:59:35 -0600

    Button Functionality added
    Side Note: Scripts need to be referenced at the very end of every html page

--- js/game.js
+++ js/game.js
@@ -1,4 +1,15 @@
+//button function initialization
+const attackButton = document.getElementById('attackBtn');
+attackButton.addEventListener('click', attack);
+const defendButton = document.getElementById('defendBtn');
+defendButton.addEventListener('click', defend);
+const fleeButton = document.getElementById('fleeBtn');
+fleeButton.addEventListener('click', flee);
+const playButton = document.getElementById('playBtn');
+playButton.addEventListener('click', play);
+
 window.onload = function() {
+  //stuff to happen on the window loading
   var i = 0;
   var titleTextHolder = "Welcome to Javascript RPG"
   var speed = 50;
@@ -10,4 +21,29 @@ window.onload = function() {
     }
   }
   titleWriter();
+  attackButton.disabled = true;
+  defendButton.disabled = true;
+  fleeButton.disabled = true;
+  playButton.disabled = false;
+
+}
+
+
+function play(){
+  attackButton.disabled = false;
+  defendButton.disabled = false;
+  fleeButton.disabled = false;
+  playButton.disabled = true;
+}
+
+function attack(){
+
+}
+
+function defend(){
+
+}
+
+function flee(){
+
 }
